64131dbe6fa28ebaa703da1e602d4b7901f27ab8
Kpaiy <craigjarrodonly@gmail.com>
2015-05-10 20:21:33 +1000

    I contributed!
    Can't shoot yourself anymore.

---
+++ Assets/C#/ignoreRaycastIfMine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ignoreRaycastIfMine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(GetComponent<NetworkView>().isMine) {
+			gameObject.layer = 2;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
